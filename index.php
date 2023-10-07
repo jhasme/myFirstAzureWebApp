a410fdb73956cf47a0aa71951a894048c529f380
jhasme <jhasme@yahoo.in>
2023-10-07 15:47:28 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>myFirstAzureWebApp</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+        }
+
+        .container {
+            max-width: 960px;
+            margin: 0 auto;
+            padding: 20px;
+        }
+
+        .hello-world {
+            text-align: center;
+            margin-top: 100px;
+            font-size: 36px;
+            color: #333;
+        }
+
+        @media (max-width: 768px) {
+            .hello-world {
+                font-size: 24px;
+            }
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="hello-world">
+            Hello, World! This is my first Azure Web App.
+        </div>
+    </div>
+</body>
+
+</html>
